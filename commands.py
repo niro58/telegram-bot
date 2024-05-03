0cfefdf43654f1a9a51de5bcbd14f3d26042520a
niro58 <nichita.roilean@gmail.com>
2024-05-03 13:27:40 +0200

    translation completed

--- commands.py
+++ commands.py
@@ -31,7 +31,7 @@ class Commands:
 
         self.logger = logging.getLogger(__name__)
 
-    async def _process_reply(self, user: User, reply_message: str):
+    async def _process_reply(self, user: User, reply_message: str, context: ContextTypes.DEFAULT_TYPE):
         next_state_key = self.data.get_next_state_key(
             user.state,
             reply_message,
@@ -43,6 +43,7 @@ class Commands:
             reply_message,
             user.language
         )
+
         self.logger.log(logging.INFO, f"Reply message: {reply_message}")
         self.logger.log(logging.INFO, f"Next state key: {next_state_key}")
         self.logger.log(logging.INFO, f"Button command: {button_command}")
@@ -66,6 +67,14 @@ class Commands:
             user.reply_message_state = user.state
         elif button_command == CommandEnum.MOVE_TO_REAL_CHAT:
             self.command_handler.move_to_real_chat()
+        elif button_command == CommandEnum.SEND_GUIDE_FILE:
+            GUIDE_PDF_PATH = "presentation_ontaxi_driver.pdf"
+            await context.bot.send_document(
+                chat_id=user.chat_id,
+                document=open(os.path.join(
+                    self.image_folder, GUIDE_PDF_PATH), "rb")
+            )
+            user.reply_message_state = next_state_key
 
         self.user_data.update_user(user)
 
@@ -75,7 +84,7 @@ class Commands:
 
         self.logger.log(logging.INFO, f"User message: {user_message}")
 
-        await self._process_reply(user, user_message)
+        await self._process_reply(user, user_message, context)
 
     async def start(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
         self.logger.log(logging.INFO, "Start command")
@@ -109,7 +118,7 @@ class Commands:
                 chat_id=update.message.chat_id,
                 photo=os.path.join(self.image_folder, user_state["image"])
             )
-        print("SENDING TEXT AFTER IMAGE")
+
         await update.message.reply_text(
             user_reply_state["text"],
             reply_markup=ReplyKeyboardMarkup(
@@ -148,13 +157,14 @@ class Commands:
                 reply_keyboard[index].append(value["text"])
             if len(reply_keyboard[index]) == max_columns:
                 index += 1
-        print(user_reply_state)
+
         if "image" in user_reply_state:
             await context.bot.send_photo(
                 chat_id=update.message.chat_id,
                 photo=os.path.join(self.image_folder,
                                    user_reply_state["image"])
             )
+
         await update.message.reply_text(
             user_reply_state["text"],
             reply_markup=ReplyKeyboardMarkup(
--- my_enums/commands.py
+++ my_enums/commands.py
@@ -5,3 +5,4 @@ class CommandEnum(Enum):
     LANGUAGE_SELECTOR = 0
     MOVE_TO_REAL_CHAT = 1
     TEXT_PRINT = 2
+    SEND_GUIDE_FILE = 3
